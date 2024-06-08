6f8a480e0380e1669d25da6fd3f276e3dd74595c
Bolelli <joaopedrobolellicandido@gmail.com>
2024-06-08 00:45:41 -0300

    Update create_tables.sql

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -1,16 +1,53 @@
 -- é aqui onde a gente vai programar TUDO UHUULLL!
 
 -- OiIIIIIIRIS FGBOD FNJKNFDGBKJNBFDA
-CREATE DATABASE OI {
-    oi int;
-    primary key(oi);
+
+CREATE TABLE pacientes{
+    id_paciente INT PRIMARY KEY,
+    nome_paciente VARCHAR(100),
+    idade_paciente INT,
+    sexo_paciente VARCHAR(15),
+    planoSaude_paciente VARCHAR(55),
+    alergias TEXT
+    diagnostico TEXT
+}
+CREATE TABLE funcionarios{
+    id_funcionario INT PRIMARY KEY,
+    nome VARCHAR(100),
+    idade INT,
+    sexo VARCHAR(15),
+    salario FLOAT,
+    cargaHoraria VARCHAR(20)
+}
+CREATE TABLE secretarias{
+    id_secretaria INT PRIMARY KEY,
+    id_funcionario INT,
+    FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id_funcionario),
+}
+CREATE TABLE enfermeiros{
+    id_enfermeiro INT PRIMARY KEY,
+    id_funcionario INT,
+    FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id_funcionario),
+}
+CREATE TABLE medicos{
+    id_medico INT PRIMARY KEY,
+    crm VARCHAR(55),
+    id_funcionario INT,
+    FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id_funcionario)
 
 }
-CREATE TABLE bomDia{
-    bomdia TEXT;
-    PRIMARY KEY(TEXT);
+CREATE TABLE exames{
+    id_exame INT PRIMARY KEY,
+    tipo_exame VARCHAR(100),
 }
-
-CREATE TABLE aloo{
-    sla int;
+CREATE TABLE consultas{
+    id_consulta INT PRIMARY KEY,
+    data_consulta DATE,
+    valor_consulta FLOAT,
+    id_paciente INT,
+    id_exames INT,
+    id_medico INT,
+    FOREIGN KEY (id_medico) REFERENCES medicos(id_medico),
+    FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
+    FOREIGN KEY (id_exames) REFERENCES exames(id_exames)
 }
